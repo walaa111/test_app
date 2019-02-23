57d3f8b813dbc811314e5239aa945d03b5cfd76a
walaa111 <walaagamil18@gmail.com>
2019-02-23 21:42:03 +0200

    add

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -28,8 +28,22 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+	//admob.initAdmob("ca-app-pub-7251676025279948/7827344712","ca-app-pub-7251676025279948/5792208319");
+	//admob.showBanner(admob.BannerSize.BANNER, admob.Position.BOTTOM_APP);
+
+	var inAppBrowserbRef = cordova.InAppBrowser.open('https://electrostar.ovplatform.tk', '_self', 'location=no,toolbar=no');
+        inAppBrowserbRef = cordova.InAppBrowser.open('http://onsport.fm/onsport/', '_self', 'location=no,toolbar=no,zoom=no');
+        /*inAppBrowserbRef.addEventListener('loadstart', inAppBrowserbLoadStart);
+        inAppBrowserbRef.addEventListener('loadstop', inAppBrowserbLoadStop);
+        inAppBrowserbRef.addEventListener('loaderror', inAppBrowserbLoadError);
+        inAppBrowserbRef.addEventListener('exit', inAppBrowserbClose);
+	*/
+	
+	//alert('end of ready');
+    
     },
 
+ 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
@@ -38,9 +52,62 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
+initAd();
+showBannerFunc();
+showInterstitialFunc();
         console.log('Received Event: ' + id);
     }
 };
+//initialize the goodies 
+function initAd(){
+        if ( window.plugins && window.plugins.AdMob ) {
+            var ad_units = {
+                ios : {
+                    banner: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx',		//PUT ADMOB ADCODE HERE 
+                    interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'	//PUT ADMOB ADCODE HERE 
+                },
+                android : {
+                    banner: 'ca-app-pub-7251676025279948/9790964383',		//PUT ADMOB ADCODE HERE 
+                    interstitial: 'ca-app-pub-7251676025279948/1321964346'	//PUT ADMOB ADCODE HERE 
+                }
+            };
+            var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
+ 
+            window.plugins.AdMob.setOptions( {
+                publisherId: admobid.banner,
+                interstitialAdId: admobid.interstitial,
+                adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,	//use SMART_BANNER, BANNER, LARGE_BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD 
+                bannerAtTop: true, // set to true, to put banner at top 
+                overlap: true, // banner will overlap webview 
+                offsetTopBar: false, // set to true to avoid ios7 status bar overlap 
+                isTesting: false, // receiving test ad 
+                autoShow: true // auto show interstitial ad when loaded 
+            });
+ 
+            registerAdEvents();
+        } else {
+            //alert( 'admob plugin not ready' ); 
+        }
+}
+//functions to allow you to know when ads are shown, etc. 
+function registerAdEvents() {
+        document.addEventListener('onReceiveAd', function(){});
+        document.addEventListener('onFailedToReceiveAd', function(data){});
+        document.addEventListener('onPresentAd', function(){});
+        document.addEventListener('onDismissAd', function(){ });
+        document.addEventListener('onLeaveToAd', function(){ });
+        document.addEventListener('onReceiveInterstitialAd', function(){ });
+        document.addEventListener('onPresentInterstitialAd', function(){ });
+        document.addEventListener('onDismissInterstitialAd', function(){ });
+    }
+ //display the banner 
+function showBannerFunc(){
+    window.plugins.AdMob.createBannerView();
+}
+//display the interstitial 
+function showInterstitialFunc(){
+    window.plugins.AdMob.createInterstitialView();	//get the interstitials ready to be shown and show when it's loaded. 
+    window.plugins.AdMob.requestInterstitialAd();
+}
 
 app.initialize();
